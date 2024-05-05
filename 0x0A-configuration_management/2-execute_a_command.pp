2676c13ca370e6e594d4917a27674d90cd6638e8
PKgothatso <ksp331@outlook.com>
2024-05-05 11:11:22 -0700

    Task 2

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,10 @@
+# A manifest to kill a running process killmenow
+
+exec { 'killing a process using pkill':
+  command  => 'pkill -9 killmenow',
+  path     => '/usr/bin:/bin',
+  onlyif   => 'pgrep killmenow',
+  provider => shell,
+
+}
+
