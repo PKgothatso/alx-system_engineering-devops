c3c5760778f1fae0f43d19962536d5b5edeae890
PKgothatso <ksp331@outlook.com>
2024-06-04 07:04:45 -0700

    Task 1-Top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,20 +4,41 @@ import requests
 
 
 def top_ten(subreddit):
-    """function that queries the Reddit API and prints the titles of the
-    first 10 hot posts listed for a given subreddit"""
+    """
+    Prints the titles of the 10 hottest posts on a given subreddit.
 
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "Google Chrome Version 125.0.6422.142"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    Args:
+        subreddit (str): Name of the subreddit to query.
+
+    Returns:
+        None: If an error occurs or the subreddit is invalid.
+    """
+
+    # Construct the URL for the subreddit's hot posts in JSON format
+    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+
+    # Define headers for the HTTP request, including User-Agent
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
+
+    # Send a GET request to the subreddit's hot posts page
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+
+        # Check for successful response (status code 200)
+        if response.status_code == 200:
+            try:
+                results = response.json()
+                children = results.get("data", {}).get("children", [])
+                [print(child.get("data", {}).get("title")) for child
+                 in children[:10]]
+            except JSONDecodeError as e:
+                print(f"Error: Failed to decode JSON response. ({e})")
+                return None
+        else:
+            # Handle non-200 status codes (e.g., 404 Not Found)
+            print("None")
+    except requests.exceptions.RequestException as e:
+        print(f"Error: Request failed. ({e})")
+        return None
+
+    return None
